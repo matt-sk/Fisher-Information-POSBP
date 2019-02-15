394a41ce80b7b608d70072b3f180a0c52690c507
Matt Skerritt <matthew.skerritt@uon.edu.au>
2019-02-15 20:38:50 +1100

    Added Fisher.so.h
    Due to an oversight, this file wasn’t added in by default. It is only needed when calling the library from another program. It contains function declarations for the functions defined in Fisher.so.cc.

---
+++ Fisher.so.h
@@ -0,0 +1,31 @@
+float FI2_f( float t1, float t2, float p, float lambda );
+
+double FI2( double t1, double t2, double p, double lambda );
+
+float FI3_f( float t1, float t2, float t3, float p, float lambda );
+
+double FI3( double t1, double t2, double t3, double p, double lambda );
+
+float FI4_f( float t1, float t2, float t3, float t4, float p, float lambda );
+
+double FI4( double t1, double t2, double t3, double t4, double p, double lambda );
+
+float FI5_f( float t1, float t2, float t3, float t4, float t5, float p, float lambda )
+
+double FI5( double t1, double t2, double t3, double t4, double t5, double p, double lambda )
+
+float FI2_threaded_f( float t1, float t2, float p, float lambda, size_t numThreads );
+
+double FI2_threaded( double t1, double t2, double p, double lambda, size_t numThreads );
+
+float FI3_threaded_f( float t1, float t2, float t3, float p, float lambda, size_t numThreads );
+
+double FI3_threaded( double t1, double t2, double t3, double p, double lambda, size_t numThreads );
+
+float FI4_threaded_f( float t1, float t2, float t3, float t4, float p, float lambda, size_t numThreads );
+
+double FI4_threaded( double t1, double t2, double t3, double t4, double p, double lambda, size_t numThreads );
+
+float FI5_threaded_f( float t1, float t2, float t3, float t4, float t5, float p, float lambda );
+
+double FI5_threaded( double t1, double t2, double t3, double t4, double t5, double p, double lambda );
